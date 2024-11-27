b24a2fb888b946d6fc3c32500d94ba3811143386
paulj <pauljouvanceau@gmail.com>
2024-11-27 09:04:16 +0100

    make the columns nullable

--- create table.sql
+++ create table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `data` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `Artist` varchar(500) NOT NULL,
-  `Album` varchar(1000) NOT NULL,
-  `Chanson` varchar(500) NOT NULL,
-  `Timestamp` varchar(500) NOT NULL,
-  `Username` varchar(500) NOT NULL,
+  `Artist` varchar(500),
+  `Album` varchar(1000),
+  `Chanson` varchar(500),
+  `Timestamp` varchar(500),
+  `Username` varchar(500),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4931683 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
