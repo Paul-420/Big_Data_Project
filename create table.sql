de1695fd8e2b1176051bdcb319f12af800b6615e
paulj <pauljouvanceau@gmail.com>
2024-11-27 08:27:15 +0100

    add create table.sql

---
+++ create table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `data` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `Artist` varchar(500) NOT NULL,
+  `Album` varchar(1000) NOT NULL,
+  `Chanson` varchar(500) NOT NULL,
+  `Timestamp` varchar(500) NOT NULL,
+  `Username` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4931683 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
